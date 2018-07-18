1e1b95856ae3976a68a3c2e5f9c328b5f33e3e73
Eric <eric.michaud99@gmail.com>
2018-07-18 14:47:25 -0700

    version 1.0 complete

--- elephant.py
+++ elephant.py
@@ -1,8 +1,39 @@
 import os
-import click
+import sys
+import time
 import pickle
+import click
 
 DATABASE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'db.p')
+
+LEVELS_TO_INTERVALS = {
+	0: 3600 * 0, 					# 0 hrs
+	1: 3600 * 24,					# 24 hrs
+	2: 3600 * 48, 					# 48 hrs
+	3: 3600 * 24 * 7, 				# 1 week
+	4: 3600 * 24 * 7 * 2, 			# 2 weeks
+	5: 3600 * 24 * 7 * 4, 			# 1 month
+	6: 3600 * 24 * 7 * 4 * 2, 		# 2 months
+	7: 3600 * 24 * 7 * 4 * 4, 		# 4 months
+	8: 3600 * 24 * 7 * 4 * 8, 		# 8 months
+	9: 3600 * 24 * 7 * 4 * 16, 		# 16 months
+	10: 3600 * 24 * 7 * 4 * 12 * 2, # 2 years
+	11: 3600 * 24 * 7 * 4 * 12 * 4, # 4 years
+}
+
+def read_cards(path):
+	"""Loads card list from database at 'path'"""
+	try:
+		with open(path, 'rb') as f:
+			cards = pickle.load(f)
+	except:
+		cards = []
+	return cards
+
+def write_cards(cards, path):
+	"""Writes card list to database at 'path', overriding previous contents"""
+	with open(path, 'wb') as f:
+		pickle.dump(cards, f)
 
 @click.group()
 def main():
@@ -13,35 +44,36 @@ def main():
 @click.argument("question")
 @click.argument("answer")
 def add(question, answer):
-	"""Create a card"""
-	try:
-		with open(DATABASE, 'rb') as f:
-			cards = pickle.load(f)
-	except:
-		with open(DATABASE, 'wb') as f:
-			cards = []
+	"""Create a card
+	
+	Examples:
+		$ elephant add "A great question?" "A great answer."
+		$ elephant add 'Ultimate answer?' 42
+	"""
+	cards = read_cards(DATABASE)
 	cards.append(
 		{	
 			'id': len(cards), 
 			'question': question,
 			'answer': answer,
+			'time_created': time.time(),
+			'level': 0,
+			'last_reviewed': time.time(),
 		}
 	)
-	with open(DATABASE, 'wb') as f:
-		pickle.dump(cards, f)
+	write_cards(cards, DATABASE)
 
 @main.command()
 @click.option("--limit", default=10,
 	help="The maximum number of cards to list")
-def view(limit):
-	"""Print some cards"""
-	try:
-		with open(DATABASE, 'rb') as f:
-			cards = pickle.load(f)
-			for card in cards[:limit]:
-				click.echo("{}. {} --> {}".format(card['id'], card['question'], card['answer']))
-	except:
-		click.echo("No cards created yet")
+def ls(limit):
+	"""List some cards"""
+	cards = read_cards(DATABASE)
+	if cards:
+		for card in cards[:limit]:
+			click.echo("{}. {} --> {}".format(card['id'], card['question'], card['answer']))
+	else:
+		click.echo("No cards available")
 
 @main.command()
 @click.option("--limit", default=15,
@@ -50,32 +82,72 @@ def view(limit):
 def search(phrase, limit):
 	"""Print cards containing phrase (case sensitive)"""
 	got_hits = False
-	try:
-		with open(DATABASE, 'rb') as f:
-			cards = pickle.load(f)
-			for card in cards[:limit]:
-				if limit <= 0:
-					break
-				if phrase in card['question'] or phrase in card['answer']:
-					got_hits = True
-					limit -= 1
-					click.echo("{}. {} --> {}".format(card['id'], card['question'], card['answer']))
-	except Exception as e:
-		click.echo("Error: {}".format(e))
+	cards = read_cards(DATABASE)
+	if cards:
+		for card in cards[:limit]:
+			if limit <= 0:
+				break
+			if phrase in card['question'] or phrase in card['answer']:
+				got_hits = True
+				limit -= 1
+				click.echo("{}. {} --> {}".format(card['id'], card['question'], card['answer']))
+	else:
+		click.echo("No cards available")
+		sys.exit()
 	if not got_hits:
 		click.echo("Found no matches")
 
 @main.command()
 def quiz():
-	"""Print cards one-by-one"""
-	try:
-		with open(DATABASE, 'rb') as f:
-			cards = pickle.load(f)
-			for card in cards:
+	"""Review cards you are at risk of forgetting"""
+	cards = read_cards(DATABASE)
+	if cards:
+		current_time = time.time()
+		todays_cards = [card for card in cards \
+			if (card['last_reviewed']+LEVELS_TO_INTERVALS[card['level']]) < current_time]
+		if not todays_cards:
+			click.echo("No cards require reviewing now")
+			sys.exit()
+		click.echo("Starting review session... Spam Ctrl-C to stop")
+		try:
+			for card in todays_cards:
 				click.echo("\nQUESTION : {} \n".format(card['question']))
-				click.prompt("Reveal?", default="Enter")
+				click.pause(info="(Press any key to show answer)")
 				click.echo("\nANSWER : {} \n".format(card['answer']))
-				if not click.confirm("Continue?", default=True):
-					break
-	except:
-		click.echo("No cards created yet")
+				mem_status = click.prompt("Did you remember it? yes/meh/no", default='yes')
+				if mem_status in ['yes', 'YES', 'y', 'Y']:
+					card['level'] += 1
+					card['last_reviewed'] = time.time()
+				elif mem_status in ['meh', 'MEH', 'm', 'M']:
+					card['level'] = int(card['level'] / 1.5)
+					card['last_reviewed'] = time.time()
+				elif mem_status in ['no', 'NO', 'n', 'N']:
+					card['level'] = 0
+					card['last_reviewed'] = time.time()
+				else:
+					click.echo("Unrecognized response. Made no change to card state")
+		except:
+			click.echo("Ending session...")
+		write_cards(cards, DATABASE)
+	else:
+		click.echo("No cards available")
+
+@main.command()
+@click.argument("ids", nargs=-1, type=int)
+def rm(ids):
+	"""Remove cards with listed ids"""
+	number_cards_removed = 0
+	cards = read_cards(DATABASE)
+	if cards:
+		i = 0
+		while i < len(cards):
+			if cards[i]['id'] in ids:
+				cards.pop(i)
+				number_cards_removed += 1
+			else:
+				i += 1
+		write_cards(cards, DATABASE)
+		click.echo("Removed {} cards".format(number_cards_removed))
+	else:
+		click.echo("No cards available")
+	
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='elephant',
-	version=0.1,
+	version=1.0,
 	install_requires=[
 		'Click'
 	],
